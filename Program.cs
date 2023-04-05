60b9f3149a5f953f4394dcff5468e83562c04a75
PavelProkopchuk <pashka.prokopchuk@gmail.com>
2023-04-05 22:43:37 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿///> Vladislav Hmilnickij:
+
+////////////////////////////////////////////
+
+///1) создать список "List" интов например от 1 до 10 и все четные числа выводить умноженными на 2, через форич
+
+///2) с помощью любого цикла заменить все нечётные числа на 1
+
+List<int> nums = new List<int>
+{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+Console.Write(" Full masive: ");
+
+foreach (int num in nums)
+{   
+    Console.Write(num + " ");
+}
+
+Console.WriteLine();
+
+Console.Write("\n Even multiplied by two: ");
+foreach (int i in nums)
+{
+    if (i % 2 == 0)
+        Console.Write(i * 2 + " ");
+}
+
+Console.WriteLine();
+
+Console.Write("\n Odd ones are replaced by one: ");
+for (int i = 0; i < nums.Count; i++)
+{
+    if (nums[i] % 2 == 1)
+    {
+        nums[i] = 1;
+        Console.Write(nums[i] + " ");
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+///3) создать новый список интов и в него записать из первово списка те значения которые больше 10 и меньше 7
+
+///4) нужно отсортировать второй список и вывести все элементы через фор
+
+List<int> firstList = new List<int>
+{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+Console.WriteLine("\nLees than 7 or more than 10: ");
+foreach (int values in firstList)
+{
+    if (values > 10)
+    {
+        Console.Write(values + " ");
+    }
+    Console.WriteLine();
+    if (values < 7)
+    {
+        Console.Write(values + " ");
+    }
+}
+
+Console.WriteLine();
+
+Console.Write("Sorting of numbers: ");
+int[] numbers = { 1, 3, 2, 8, 7, 15, 5, 9, 6, 13, 11, 12, 4, 10 };
+var orderedNumbers = from i in numbers
+                     orderby i
+                     select i;
+foreach (int i in orderedNumbers)
+    Console.Write(i + " ");
+
+Console.WriteLine();
